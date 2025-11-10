605e8139c161e6191193c7badc063715b21445ab
Human <aptech.daniyal0@gmail.com>
2025-11-10 17:02:14 +0500

    Java-Switch Case-Loops

--- JavaScript Codes/Js/Conditional-Statement.js
+++ JavaScript Codes/Js/Conditional-Statement.js
@@ -1,23 +1,23 @@
 console.log("IF - ElseIf - Else | Statement");
 
 //If - Else
-let input = prompt("Your Current Age", "");
-console.log(input);
-if (input >= 18) {
-    document.write("You are 18+ Verified");
-}
-else {
-    document.write("You are 18- Verification Failed");
-}
+// let input = prompt("Your Current Age", "");
+// console.log(input);
+// if (input >= 18) {
+//     document.write("You are 18+ Verified");
+// }
+// else {
+//     document.write("You are 18- Verification Failed");
+// }
 
 //If - ElseIf - Else
 let inp = prompt("Your Current Age", "");
-console.log(input);
-if (inp > 17) {
-    document.write("Proceed");
+console.log(inp);
+if (inp >= 19) {
+    document.write("Proceed 19+");
 }
-else if (inp = 18) {
-    document.write("Verified-Proceed");
+else if (inp == "") {
+    document.write("Add Your Age");
 }
 else {
     document.write("Cant-Proceed");
