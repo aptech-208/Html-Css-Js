5e6779fc18696640d51554511ec199f04e30001d
Human <aptech.daniyal0@gmail.com>
2025-11-12 17:03:46 +0500

    Document Object Model

--- JavaScript Codes/Js/Loop-While.js
+++ JavaScript Codes/Js/Loop-While.js
@@ -1,8 +1,8 @@
-// let i = prompt("Enter a starting number: ");
-// while (i <= 20) {
-//     document.write("The number is: " + i + "<br>");
-//     i++;
-// }
+let i = prompt("Enter a starting number: ");
+while (i <= 20) {
+    document.write("The number is: " + i + "<br>");
+    i++;
+}
 
 // Infinite Loop
 // while (true) {
@@ -17,6 +17,6 @@
 // while (j <= 20);
 
 
-for (let k = prompt("Enter a starting number for loop: "); k <= 20;) k++ {
-    document.write("The number in for loop is: " + k + "<br>");
-}
+// for (let k = prompt("Enter a starting number for loop: "); k <= 20;) k++ {
+//     document.write("The number in for loop is: " + k + "<br>");
+// }
