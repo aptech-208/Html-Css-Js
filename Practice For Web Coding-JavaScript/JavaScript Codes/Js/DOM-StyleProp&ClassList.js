5346b7a786ab4da3ab3b0480081a9aa0ff328189
Human <aptech.daniyal0@gmail.com>
2025-11-17 17:13:10 +0500

    Events With Js

--- Practice For Web Coding-JavaScript/JavaScript Codes/Js/DOM-StyleProp&ClassList.js
+++ Practice For Web Coding-JavaScript/JavaScript Codes/Js/DOM-StyleProp&ClassList.js
@@ -5,5 +5,5 @@ const overight = document.querySelector(".bg").classList.remove("bg");
 const para = document.querySelector(".bbg").style.backgroundColor = "green";
 
 //Non Single Line
-const para = document.querySelector(".bbg")
+const para1 = document.querySelector(".bbg")
 para.style.backgroundColor = "red";
